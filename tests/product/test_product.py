79a166768881b43d5f57fe69c12f2e88459bb597
Francisca Ribeiro <ribeirofrv@gmail.com>
2023-02-18 23:20:35 -0300

    test: inicia test_cria_produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Arroz",
+        "Arroz do Brasil",
+        "2020-01-01",
+        "2022-01-01",
+        "123456789",
+        "em local seco",
+    )
+    assert product.id == 1
+    assert product.nome_do_produto == "Arroz"
+    assert product.nome_da_empresa == "Arroz do Brasil"
+    assert product.data_de_fabricacao == "2020-01-01"
+    assert product.data_de_validade == "2022-01-01"
+    assert product.numero_de_serie == "123456789"
+    assert product.instrucoes_de_armazenamento == "em local seco"
