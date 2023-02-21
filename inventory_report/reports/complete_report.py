6faf69a1bd361901a552ad133f272fa1fa8d94a5
Francisca Ribeiro <ribeirofrv@gmail.com>
2023-02-21 01:14:35 -0300

    feat: cria class para gerar relatorio completo

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,26 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(stock):
+        simple_report = SimpleReport.generate(stock)
+
+        # products stocked by company -> name of company: number of products
+        products_by_company = {}
+        for item in stock:
+            company = item["nome_da_empresa"]
+            if company in products_by_company:
+                products_by_company[company] += 1
+            else:
+                products_by_company[company] = 1
+
+        companies = ""
+        for company, number in products_by_company.items():
+            companies += f"- {company}: {number}\n"
+
+        return (
+            f"{simple_report}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{companies}"
+        )
