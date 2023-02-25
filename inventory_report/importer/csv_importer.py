e5966fd34831fa05520e6f97cc24c428f72ac979
Francisca Ribeiro <ribeirofrv@gmail.com>
2023-02-25 13:54:14 -0300

    fix: raise Error quando arquivo inválido -CsvImporter

--- inventory_report/importer/csv_importer.py
+++ inventory_report/importer/csv_importer.py
@@ -5,9 +5,11 @@ from inventory_report.importer.importer import Importer
 class CsvImporter(Importer):
     @staticmethod
     def import_data(path: str):
-        try:
-            with open(path, encoding="utf-8") as csv_file:
-                reader = csv.DictReader(csv_file, delimiter=",", quotechar='"')
-                return list(reader)
-        except Exception:
+        if "csv" not in path:
             raise ValueError("Arquivo inválido")
+        # try:
+        with open(path, encoding="utf-8") as csv_file:
+            reader = csv.DictReader(csv_file, delimiter=",", quotechar='"')
+            return list(reader)
+        # except Exception:
+        #     raise ValueError("Arquivo inválido")
