163e09a6b4b8621433672a87f42d0fac4752c863
Francisca Ribeiro <ribeirofrv@gmail.com>
2023-02-25 14:23:46 -0300

    test: testa relatório individual do produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        id=1,
+        nome_do_produto="farinha",
+        nome_da_empresa="Farinini",
+        data_de_fabricacao="2021-05-01",
+        data_de_validade="2023-06-02",
+        numero_de_serie="123456789",
+        instrucoes_de_armazenamento="ao abrigo da luz",
+    )
+
+    assert f"{product}" == (
+        "O produto farinha"
+        " fabricado em 2021-05-01"
+        " por Farinini com validade"
+        " até 2023-06-02"
+        " precisa ser armazenado ao abrigo da luz."
+    )
